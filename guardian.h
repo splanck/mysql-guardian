b0e6254778177c4e1fa50cd901c07e8adedd4ca2
Stephen Planck <splanck9271@gmail.com>
2018-07-16 11:16:34 +1000

    Added GPL notice to all source files.

--- guardian.h
+++ guardian.h
@@ -1,3 +1,22 @@
+/*
+	Copyright (c) 2018 - Stephen Planck and Alistair Packer
+
+	This file is part of MySQL Guardian.
+
+    MySQL Guardian is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    any later version.
+
+    MySQL Guardian is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MySQL Guardian. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 void initialiseLog();
 void cleanUpTasks();
 void getConfig();
