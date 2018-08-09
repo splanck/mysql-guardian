02271f4a950278b15d7e1b224dc682eff38c49fc
Stephen Planck <splanck9271@gmail.com>
2018-08-10 03:23:56 +1000

    Removed some unneeded includes from utility.c

--- utility.c
+++ utility.c
@@ -19,20 +19,17 @@
     along with MySQL Guardian. If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
-#include <netinet/ip_icmp.h>
 #include <time.h>
 #include <fcntl.h>
-#include <signal.h>
-#include <time.h>
 #include "utility.h"
 
 struct myserver *pFirst = NULL;
