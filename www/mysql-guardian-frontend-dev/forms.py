1591aa3f577156adf70dbeb96fb8f16914230ae0
alistair <al@pepe.home>
2018-12-08 22:52:44 +1000

    new format and base code

---
+++ www/mysql-guardian-frontend-dev/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class SignupForm(Form):
+    first_name = StringField ('First Name')
+    last_name = StringField ('Last Name')
+    email = StringField('Email')
+    password = PasswordField('Password')
+    submit = SubmitField ('Sign up')
