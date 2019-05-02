e91509512921a37db7e366a4c74dda597fa8c5e7
brodzig <packer.alistair@gmail.com>
2019-05-02 17:35:32 +1000

    more setup

--- www/routes.py
+++ www/routes.py
@@ -34,6 +34,10 @@ def guardian_servers():
   get_guardian_servers = Guardian_servers.query.all()
   return render_template("guardian_servers.html", get_guardian_servers = get_guardian_servers )
 
+@app.route("/add_user")
+def add_user():
+  return render_template("add_user.html")
+
 @app.route("/signup", methods = ['GET', 'POST'])
 def signup():
   form = SignupForm()
