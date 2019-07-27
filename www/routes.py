a2cb014012f94b0bf0f592c8d026beae804686c0
alistair <Alistair>
2019-07-27 12:24:48 +1000

    setup for performing checks

--- www/routes.py
+++ www/routes.py
@@ -33,6 +33,10 @@ def guardian_servers():
   get_guardian_servers = Guardian_servers.query.all()
   return render_template("guardian_servers.html", get_guardian_servers = get_guardian_servers )
 
+@app.route("/guardian_checks")
+def guardian_checks():
+  return render_template("guardian_checks.html")
+
 @app.route("/add_user", methods = ['GET', 'POST'])
 def add_user():
   form = AddUser()
